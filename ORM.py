9823b1923019ffeb96563f5582bcb08812986a10
Kevin <servidoryei@gmail.com>
2018-03-03 15:07:13 -0600

    añadi los atributos a la clase categoria

--- ORM.py
+++ ORM.py
@@ -1,9 +1,12 @@
 class categoria:
-	def __init__(self):
+	def __init__(self,_id,_nombre,_desc,_status):
+		self.id=_id
+		self.nombre=_nombre
+		self.desc=_desc
+		self.status=_status
 		pass
-
 	def __JSON__(self):
-		return "{ \"id\" : "+self.id+"},\n"+"\"nombre\":\""+self.nombre+"\",\n"+"\"status\":\""+self.status+"\"}"
+		return "{ \"id\" : "+self.id+"},\n"		+		"\"nombre\":\""+self.nombre+"\",\n"		+		"\"status\":\""+self.status+"\",}"
 
 class cliente:
 	pass
