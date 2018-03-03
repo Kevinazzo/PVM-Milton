c41958fded96d4033741f90a4c0859996d790454
Kevin <servidoryei@gmail.com>
2018-03-03 17:19:55 -0600

    Intento de consulta que no sirve tampoco sirve el patron de URL

--- ORM.py
+++ ORM.py
@@ -1,12 +1,11 @@
 class categoria:
 	def __init__(self,_id,_nombre,_desc,_status):
-		self.id=_id
-		self.nombre=_nombre
-		self.desc=_desc
-		self.status=_status
-		pass
+		self.id = _id
+		self.nombre = _nombre
+		self.desc = _desc
+		self.status = _status
 	def __JSON__(self):
-		return "{ \"id\" : "+self.id+"},\n"		+		"\"nombre\":\""+self.nombre+"\",\n"		+		"\"status\":\""+self.status+"\",}"
+		return "{ \"id\" : " + self.id + "},\n" + "\"nombre\":\"" + self.nombre + "\",\n" + "\"status\":\"" + self.status + "\",}"
 
 class cliente:
 	pass
@@ -19,5 +18,7 @@ class sucursal:
 class colonia:
     pass
 class empresa:
-    david=david
+	pass
+
+class ciudad:
 	pass
